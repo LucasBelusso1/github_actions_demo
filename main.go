ff5fd525f413237a77e4de44cfe2fe54ceb4e6d4
LucasBelusso1 <belussolucas@gmail.com>
2025-01-28 20:53:19 -0300

    feat: Beautifies main.go

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-
-	fmt.Println(sum(1, 2))
+	fmt.Println("Resultado da soma de 1 + 2:", sum(1, 2))
 }
 
 func sum(a, b int) int {
